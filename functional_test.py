ac3b2c3adfc7f21056eb990d959491b1a65499f4
prai05 <prai05@hotmail.com>
2018-03-29 13:32:05 +0700

    crate functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def teatDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Prai go to homepage
+        self.browser.get('http://localhost:8000')
+
+        # page title 
+        self.assertIn('Quiz', self.browser.title)
+        self.fail('Finish the test!')
+
+        # questions and ans
+
+        # score true false
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
