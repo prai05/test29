0bb48bc8c6b31f98398d37f5e6a43efe353a9a77
prai05 <prai05@hotmail.com>
2018-06-17 23:38:11 +0700

    add database (quiz & choice)

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from django.http import HttpRequest
+from quiz.views import index
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = index(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>quiz</title>', html)  
+        self.assertTrue(html.strip().endswith('</html>'))
+    
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, '/index.html')
