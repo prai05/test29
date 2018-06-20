824cb5747bec5bdd8313b471adbdcf0fb8fbbb50
prai05 <prai05@hotmail.com>
2018-06-20 12:16:57 +0700

    add admin manage Question&Choice

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
-# from .models import Question, Choice
+from .models import Question, Choice
+
 # # Register your models here.
-# admin.site.site_title = 'Quiz'
-# admin.site.site_header = 'Quiz Administration'
-#
+
+admin.site.site_title = 'Quiz'
+admin.site.site_header = 'Quiz Administration'
+
 # class QuestionAdmin(admin.ModelAdmin):
-#     list_display = ['question_text', 'created', 'updated']
-#
-# class ChoiceAdmin(admin.ModelAdmin):
-#     list_display = ['question_text', 'created', 'updated']
-#
-# admin.site.register(Question, QuestionAdmin)
-# admin.site.register(Choice, ChoiceAdmin)
+#     fields = ['created', 'question_text']
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    # radio_fields = {"Choice": admin.VERTICAL}
+    # radio_fields = {"group": admin.VERTICAL}
+    # widgets = {
+    #         'corrected': corrected.RadioSelect
+    #     }
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    inlines = [ChoiceInline]
+    # radio_fields = {"quiz": admin.VERTICAL}
+
+admin.site.register(Question, QuestionAdmin)
--- quiz/views.py
+++ quiz/views.py
@@ -19,27 +19,16 @@ def question(request):
 
 def results(request):
     if request.method =='POST':
-        question_text = Question.objects.all() #.order_by('-id')
+        question_text = Question.objects.all()
         question_num = len(question_text)
+        choice = Choice.objects.all()
         point = 0
-        choice1 = Choice.objects.get(pk=1)
-        choice2 = Choice.objects.get(pk=2)
-        choice3 = Choice.objects.get(pk=3)
-        radio1 = request.POST.get('choice1')
-        radio2 = request.POST.get('choice2')
-        radio3 = request.POST.get('choice3')
         for i in range(question_num):
-            if str(Choice.objects.get(pk=i+1).corrected) == request.POST.get('choice' + str(i+1)):
+            if str(choice[i].corrected) == request.POST.get('choice'+str(i+1)):
                 point += 1
         context = {
             'point': point,
             'question_num': question_num,
             'question_text': question_text,
-            'choice1': choice1,
-            'choice2': choice2,
-            'choice3': choice3,
-            'radio1': radio1,
-            'radio2': radio2,
-            'radio3': radio3,
         }
         return render(request, 'results.html', context)
